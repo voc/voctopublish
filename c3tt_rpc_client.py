09a9099583fd0b52405c4439534ee73cc91310c2
derpeter <sites@derpeter.net>
2016-12-09 19:33:51 +0100

    renamed c3t to c3tt fixed some shadowing variables changed functions names to lowercase

--- c3tt_rpc_client.py
+++ c3tt_rpc_client.py
@@ -25,7 +25,7 @@ import logging
 from ticket_module import Ticket
 
 
-class C3TClient:
+class C3TTClient:
     """
     group: worker group
     secret: client secret
@@ -89,7 +89,7 @@ class C3TClient:
             msg = "A fault occurred\n"
             msg += "Fault code: %d \n" % err.faultCode
             msg += "Fault string: %s" % err.faultString
-            raise C3TTExcption(msg) from err
+            raise C3TTException(msg) from err
 
         except xmlrpc.client.ProtocolError as err:
             msg = "A protocol error occurred\n"
@@ -97,17 +97,12 @@ class C3TClient:
             msg += "HTTP/HTTPS headers: %s\n" % err.headers
             msg += "Error code: %d\n" % err.errcode
             msg += "Error message: %s" % err.errmsg
-            raise C3TTExcption(msg) from err
+            raise C3TTException(msg) from err
 
         except socket.gaierror as err:
             msg = "A socket error occurred\n"
             msg += err
-            raise C3TTExcption(msg) from err
-
-        except xmlrpc.client.ProtocolError as err:
-            msg = "A Protocol occurred\n"
-            msg += err
-            raise C3TTExcption(msg) from err
+            raise C3TTException(msg) from err
 
         args.append(self.__gen_signature(method, args))
 
@@ -117,36 +112,35 @@ class C3TClient:
         except xml.parsers.expat.ExpatError as err:
             msg = "A expat err occured\n"
             msg += err
-            raise C3TTExcption(msg) from err
+            raise C3TTException(msg) from err
         except xmlrpc.client.Fault as err:
             msg = "A fault occurred\n"
             msg += "Fault code: %d\n" % err.faultCode
             msg += "Fault string: %s" % err.faultString
-            raise C3TTExcption(msg) from err
+            raise C3TTException(msg) from err
         except xmlrpc.client.ProtocolError as err:
             msg = "A protocol error occurred\n"
             msg += "URL: %s\n" % err.url
             msg += "HTTP/HTTPS headers: %s\n" % err.headers
             msg += "Error code: %d\n" % err.errcode
             msg += "Error message: %s" % err.errmsg
-            raise C3TTExcption(msg) from err
+            raise C3TTException(msg) from err
         except OSError as err:
             msg = "A OS error occurred\n"
-            msg += "Error code: %d\n" % err.errcode
-            msg += "Error message: %s" % err.errmsg
-            raise C3TTExcption(msg) from err
+            msg += "Error message: %s" % err
+            raise C3TTException(msg) from err
 
         return result
 
-    def getVersion(self):
+    def get_version(self):
         """
         get Tracker Version
         :return:
         """
-        tmp_args = ["1"];
+        tmp_args = ["1"]
         return str(self.__open_rpc("C3TT.getVersion", tmp_args))
 
-    def assignNextUnassignedForState(self, from_state, to_state):
+    def assign_next_unassigned_for_state(self, from_state, to_state):
         """
         check for new ticket on tracker an get assignement
         :param from_state:
@@ -161,14 +155,14 @@ class C3TClient:
         else:
             return ret['id']
 
-    def setTicketProperties(self, id, properties):
+    def set_ticket_properties(self, ticket_id, properties):
         """
         set ticket properties
-        :param id:
+        :param ticket_id:
         :param properties:
         :return:
         """
-        tmp_args = [id, properties]
+        tmp_args = [ticket_id, properties]
         ret = self.__open_rpc("C3TT.setTicketProperties", tmp_args)
         if not ret:
             logging.error("no xml in answer")
@@ -176,13 +170,13 @@ class C3TClient:
         else:
             return True
 
-    def getTicketProperties(self, id):
+    def get_ticket_properties(self, ticket_id):
         """
         get ticket properties
-        :param id:
+        :param ticket_id:
         :return:
         """
-        tmp_args = [id]
+        tmp_args = [ticket_id]
         ret = self.__open_rpc("C3TT.getTicketProperties", tmp_args)
         if not ret:
             logging.error("no xml in answer")
@@ -190,27 +184,27 @@ class C3TClient:
         else:
             return ret
 
-    def setTicketDone(self, id):
+    def set_ticket_done(self, ticket_id):
         """
         set Ticket status on done
-        :param id:
+        :param ticket_id:
         :return:
         """
-        tmp_args = [id]
+        tmp_args = [ticket_id]
         ret = self.__open_rpc("C3TT.setTicketDone", tmp_args)
         logging.debug(ret)
 
-    def setTicketFailed(self, id, error):
+    def set_ticket_failed(self, ticket_id, error):
         """
         set ticket status on failed an supply a error text
-        :param id:
+        :param ticket_id:
         :param error:
         :return:
         """
         enc_error = error.encode('ascii', 'xmlcharrefreplace')
-        tmp_args = [id, enc_error]
+        tmp_args = [ticket_id, enc_error]
         self.__open_rpc("C3TT.setTicketFailed", tmp_args)
 
 
-class C3TTExcption(Exception):
+class C3TTException(Exception):
     pass
