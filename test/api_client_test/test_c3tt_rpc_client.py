3f679654165a8dd01c71533943e97f3d9912e722
derpeter <sites@derpeter.net>
2017-09-21 23:12:29 +0200

    renamed src folder to voctopublish added missing __init__.py files to fix includes in tests and make unitest work added missing property to testcase

--- test/api_client_test/test_c3tt_rpc_client.py
+++ test/api_client_test/test_c3tt_rpc_client.py
@@ -5,22 +5,22 @@ Created on Dec 15, 2016
 '''
 import unittest
 
-from api_client.c3tt_rpc_client import C3TTClient
+from voctopublish.api_client.c3tt_rpc_client import C3TTClient
 
 
 class TestC3TTClient(unittest.TestCase):
-
     def setUp(self):
         self._client = C3TTClient("<server>", "group", "host", "secret")
 
     def test_init(self):
-        assert self._client.url == "<server>rpc" # todo shouldn't this be an _url join?
+        assert self._client.url == "<server>rpc"  # todo shouldn't this be an _url join?
         assert self._client.ticket_id is None
 
     def test_gen_signature_args_empty(self):
         hash_ = self._client._gen_signature("test", [])
         assert hash_ == "35cea736060974b7e67f95d7c07a2cecdc8fff882632cb73121a3b3a77a2625a"
 
+
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
