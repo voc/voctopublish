ad33df8f0047113e329ce51455ebfc0c80b7f02d
derpeter <sites@derpeter.net>
2017-12-12 23:51:18 +0100

    updated year

--- voctopublish/voctopublish.py
+++ voctopublish/voctopublish.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#    Copyright (C) 2016  derpeter
+#    Copyright (C) 2017  derpeter
 #    derpeter@berlin.ccc.de
 #
 #    This program is free software: you can redistribute it and/or modify
