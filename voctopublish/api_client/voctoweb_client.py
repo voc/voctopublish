12af666f213c66ffcdf134d4dc3380593064f1fc
derpeter <sites@derpeter.net>
2019-02-19 22:50:41 +0100

    more debug output for thumb generation

--- voctopublish/api_client/voctoweb_client.py
+++ voctopublish/api_client/voctoweb_client.py
@@ -102,6 +102,7 @@ class VoctowebClient:
                 scores = {}
                 interval = 180
                 candidates = [20, 30, 40]  # some fixed candidates we always want to hit
+                logging.debug("length of video used for thumbnail generation " + length)
                 candidates.extend(list(range(15, length - 60, interval)))  # pick some more candidates based on the file length
                 try:
                     for pos in candidates:
