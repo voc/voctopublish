fca085da6253e6c177650811fafc62f04210f9ca
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2020-03-02 18:26:44 +0100

    added more function of the tracker api

--- voctopublish/c3tt-admin.py
+++ voctopublish/c3tt-admin.py
@@ -7,6 +7,10 @@ import socket
 from api_client.c3tt_rpc_client import C3TTClient
 
 class C3TT_Admin:
+    """
+    cli helper to talk to the c3tt. Api definition can be found here
+    https://github.com/crs-tools/tracker/blob/master/src/Application/Controller/XMLRPC/Handler.php
+    """
     def __init__(self):
         if not os.path.exists('client.conf'):
             raise IOError("Error: config file not found")
@@ -33,12 +37,30 @@ class C3TT_Admin:
         ret = self.c3tt.create_encoding_ticket(ticket, profile)
         print(ret)
 
+    def get_version(self):
+        ret = self.c3tt.get_version()
+        print(ret)
+
+    def set_ticket_properties(self, ticket, properties):
+        print(properties)
+        ret = self.c3tt.set_ticket_properties(ticket, properties)
+        print(ret)
+
+    def get_ticket_properties(self, ticket):
+        ret = self.c3tt.get_ticket_properties(ticket)
+        print(ret)
+
+    def set_ticket_done(self, ticket):
+        ret = self.c3tt.set_ticket_done(ticket)
+        print(ret)
 
 parser = argparse.ArgumentParser(description="Modify tickets in the ticket tracker")
 
-parser.add_argument("task", help="task to perform: add-profile, tbd")
+parser.add_argument("task", help="task to perform: add-profile, get-properties, set-done")
 parser.add_argument("--ticket", type=int)
 parser.add_argument("--profile", type=int)
+# from https://gist.github.com/vadimkantorov/37518ff88808af840884355c845049ea
+parser.add_argument('--prop', help="each property needs to be added as an touple like: --prop foo:bar", action = type('', (argparse.Action, ), dict(__call__ = lambda a, p, n, v, o: getattr(n, a.dest).update(dict([v.split('=')])))), default = {}) # anonymously subclassing argparse.Action
 args = parser.parse_args()
 
 admin = C3TT_Admin()
@@ -46,5 +68,10 @@ admin = C3TT_Admin()
 print("Selected task: " + args.task)
 if args.task == "task=add-profile":
     admin.add_encoding_profile(args.ticket, args.profile)
-
+elif args.task == "task=get-properties":
+    admin.get_ticket_properties(args.ticket)
+elif args.task == "task=set-properties":
+    admin.set_ticket_properties(args.ticket, args.prop)
+elif args.task == "task=set-done":
+    admin.set_ticket_done(args.ticket)
 
