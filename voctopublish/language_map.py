a8d7fbed8cb95981a056ada60e62059236183e9f
derpeter <sites@derpeter.net>
2021-01-21 22:48:53 +0100

    added intial version of common language map

---
+++ voctopublish/language_map.py
@@ -0,0 +1,26 @@
+lang_map = {'deu': 'German',
+            'eng': 'English',
+            'spa': 'Spanish',
+            'gsw': 'Schweizerdeutsch',
+            'fra': 'French',
+            'rus': 'Russian',
+            'fas': 'Farsi',
+            'chi': 'Chinese',
+            'ara': 'Arabic',
+            'hrv': 'Croatian',
+            'pol': 'Polish'
+            }
+
+translation_strings = {'deu': 'Deutsche Übersetzung',
+                       'eng': 'english translation',
+                       'spa': 'La traducción española',
+                       'gsw': 'Schwizerdüütschi Übersetzig',
+                       'fra': 'traduction française', 'rus': 'Russian (русский) translation',
+                       'chi': '中文翻译',
+                       'ara': 'الترجمة العربية',
+                       'hrv': 'Hrvatski prijevod',
+                       'pol': 'Prijevod s poljskog'
+                       }
+
+lang_2to3_map = {'en': 'eng',
+                 'de': 'deu'}
