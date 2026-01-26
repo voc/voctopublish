40ed1b84d4909039e892825ee27ea7e7458a7e3e
Andi <andi@saerdnaer.de>
2026-01-26 20:25:08 +0100

    cleanup + check if youtube_url’s are published

--- scripts/sync-tracker-with-hub.py
+++ scripts/sync-tracker-with-hub.py
@@ -8,6 +8,7 @@ from os import getenv, path
 sys.path.append(path.dirname(path.dirname(__file__)))
 sys.path.append(path.dirname(__file__) + '/../voctopublish')
 
+from voctopublish.api_client.youtube_client import YoutubeAPI
 from voctopublish.voctopublish import Worker as TrackerClient
 import voctopublish.api_client.webhook_client as webhook
 
@@ -42,36 +43,54 @@ def sync_tracker_with_hub(project, args):
             print(voctoweb_url)
             ticket_url = f'https://tracker.c3voc.de/{project}/ticket/{row["ticket_id"]}'
             print(ticket_url)
-            #hub_api_url = f'https://api.events.ccc.de/congress/2025/event/{row["fahrplan_guid"]}/schedule'
-            #hub_event = requests.get(hub_api_url).json()
-            #print(hub_event['url'])
+
             print()
 
             #print(ticket)
-
-
-            # preview webhook payload
-            print(json.dumps(webhook._get_json(ticket), indent=2))
-
-
-            exit()
-
+            changed_properties = {}
 
             if ticket.master or not ticket.webhook_only_master:
-                result = webhook.send(ticket)
-                if (
-                    not isinstance(result, int) or result >= 300
-                ) and ticket.webhook_fail_on_error:
-                    raise Exception(
-                        f"POSTing webhook to {ticket.webhook_url} failed with http status code {result}"
-                    )
-                elif isinstance(result, int):
-                    tracker.c3tt.set_ticket_properties(
-                        ticket.ticket_id,
-                        {
-                            "Webhook.StatusCode": result,
-                        },
-                    )
+                
+                for key, url in ticket.youtube_urls.items():
+
+                    youtube_metadata = YoutubeAPI.get_oembed_json(url)
+
+                    # when privacy is private and we got a 200 status, change to public in ticket model so webhook can update hub
+                    if ticket.youtube_privacy == 'private' and key == 'YouTube.Url0' and youtube_metadata['status'] == 200:
+                        ticket.youtube_privacy = 'public'
+
+                    if ticket[key + '.status'] != youtube_metadata['status']:
+                        changed_properties[f'{key}.status'] = youtube_metadata['status']
+                    
+                    #if 'title' in youtube_metadata and ticket[key + '.Title'] != youtube_metadata['title']:
+                    #    changed_properties[f'{key}.Title'] = youtube_metadata['title']
+
+                print(changed_properties)
+
+                if args.debug:
+                    # preview webhook payload
+                    print(json.dumps(webhook._get_json(ticket), indent=2))
+
+                if not args.dry_run:
+                    result = webhook.send(ticket)
+                    if (
+                        not isinstance(result, int) or result >= 300
+                    ):
+                        logger.error(
+                            f"POSTing webhook to {ticket.webhook_url} failed with http status code {result}"
+                        )
+                    elif isinstance(result, int):
+                        changed_properties['Webhook.StatusCode'] = result
+
+                    if changed_properties.__len__() > 0:
+                        tracker.c3tt.set_ticket_properties(ticket.id, changed_properties)
+                        pass
+
+
+
+            print()
+            print(ticket['Fahrplan.URL'])
+
 
 
 if __name__ == "__main__":
